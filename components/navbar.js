b30a8ae64ac0b56b2af146ba7a7a306450e5a6b1
desshub <desshub95@gmail.com>
2022-02-11 03:04:09 +0300

    Navbar desings

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,13 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { useState } from 'react';
 
 function NavLink({ to, children }) {
   return (
-    <a href={to} className={`mx-4`}>
+    <a
+      href={to}
+      className={`flex items-center justify-center h-20 px-2 mx-0 w-28 hover:bg-blue-900 hover:text-blue-200 hover:border-t-blue-200 hover:border-t`}
+    >
       {children}
     </a>
   );
@@ -10,15 +15,15 @@ function NavLink({ to, children }) {
 function MobileNav({ open, setOpen }) {
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
+      className={`absolute top-0 left-0 h-screen w-screen bg-blue-800 transform ${
         open ? '-translate-x-0' : '-translate-x-full'
       } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
     >
-      <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20">
+      <div className="flex items-center justify-center ml-4 filter drop-shadow-md bg-blue-800 h-20">
         {' '}
         {/*logo container*/}
-        <a className="text-xl font-semibold" href="/">
-          LOGO
+        <a className="text-xl font-semibold" href="/" onClick={setOpen}>
+          UPTICK
         </a>
       </div>
       <div className="flex flex-col ml-4">
@@ -31,6 +36,16 @@ function MobileNav({ open, setOpen }) {
           onClick={setOpen}
         >
           Contact
+        </a>
+        <a className="text-xl font-medium my-4" href="/about" onClick={setOpen}>
+          Manage Account
+        </a>
+        <a
+          className="text-xl font-normal my-4"
+          href="/contact"
+          onClick={setOpen}
+        >
+          Sign Out
         </a>
       </div>
     </div>
@@ -47,33 +62,36 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-20 items-center">
+    <nav className="flex filter drop-shadow-md bg-blue-800 text-blue-300 px-4 h-20 items-center">
       <MobileNav open={open} setOpen={handleToggle} />
       <div className="w-3/12 flex items-center">
-        <a className="text-2xl font-semibold" href="/">
-          LOGO
+        <a
+          className="text-2xl font-semibold h-20 px-2 flex items-center hover:bg-blue-900 hover:text-blue-200"
+          href="/"
+        >
+          <span>UPTICK</span>
         </a>
       </div>
-      <div className="w-9/12 flex justify-end items-center">
+      <div className="w-9/12 flex justify-end items-center ">
         <div
-          className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden"
+          className="z-50 flex relative w-8 h-8 flex-col justify-between items-center text-blue-300 md:hidden"
           onClick={handleToggle}
           role="button"
           aria-hidden="true"
         >
           {/* hamburger button */}
           <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+            className={`h-1 w-full bg-blue-300 rounded-lg transform transition duration-300 ease-in-out ${
               open ? 'rotate-45 translate-y-3.5' : ''
             }`}
           />
           <span
-            className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
+            className={`h-1 w-full bg-blue-300 rounded-lg transition-all duration-300 ease-in-out ${
               open ? 'w-0' : 'w-full'
             }`}
           />
           <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+            className={`h-1 w-full bg-blue-300 rounded-lg transform transition duration-300 ease-in-out ${
               open ? '-rotate-45 -translate-y-3.5' : ''
             }`}
           />
@@ -82,6 +100,19 @@ export default function Navbar() {
         <div className="hidden md:flex">
           <NavLink to="/contact">CONTACT</NavLink>
           <NavLink to="/about">ABOUT</NavLink>
+          <div className="flex items-center justify-center h-20 px-2 mx-0 w-28 hover:bg-blue-900 hover:text-blue-200 hover:border-t-blue-200 hover:border-t">
+            <Menu>
+              <MenuButton className="bg-blue-800">
+                Oscar <ChevronDownIcon />
+              </MenuButton>
+              <MenuList backgroundColor="#1e40af">
+                <MenuItem className="bg-blue-800 hover:bg-blue-900">
+                  Manage Account
+                </MenuItem>
+                <MenuItem className="hover:bg-blue-900">Sign Out</MenuItem>
+              </MenuList>
+            </Menu>
+          </div>
         </div>
       </div>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 }
